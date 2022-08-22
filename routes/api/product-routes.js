35c248c05ce8da9316f682c88777d21ef239fc9f
Swagnarok <darryl.le.630@gmail.com>
2022-08-21 20:15:33 -0700

    product routes for get all and get one complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{model: Category, Tag}]
+  }).then(products => {
+    res.status(200).json(products)
+  }).catch(error => res.status(500).json(error))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [{model: Category, Tag}]
+  }).then(products => {
+    res.status(200).json(products)
+  }).catch(error => res.status(500).json(error))
 });
 
 // create new product
