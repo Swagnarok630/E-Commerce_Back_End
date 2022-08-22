58a708c018b31a59d26f974856c744602a7f39a8
Swagnarok <darryl.le.630@gmail.com>
2022-08-22 00:07:25 -0700

    minor adjustments and double checked routes

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
-      port: process.env.PORT || 3306,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
